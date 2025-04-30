71d4f45c1dfce5e0b0315190a3f71d7d7b63ee5b
egoitzaulestia <egoitzaulestia@gmail.com>
2025-04-30 02:14:46 +0200

    HW2 done

--- js/config.example.js
+++ js/config.example.js
@@ -5,3 +5,6 @@ const CONFIG = {
     TMDB_API_KEY: 'paste_your_api_key_here',
     TMDB_AUTHORIZATION_TOKEN: 'paste_your_authorization_token_here'
 }
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+axios.defaults.headers.common['Authorization'] = `Bearer ${CONFIG.TMDB_AUTHORIZATION_TOKEN}`;
